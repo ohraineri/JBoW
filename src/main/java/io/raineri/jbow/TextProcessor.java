c2665ecc4b7a9fec911bb29a6ccf45865e46d2b0
Marcelo Raineri <mraineridev@gmail.com>
2025-12-26 00:44:04 -0300

    feat: implement TextProcessor class for text manipulation

---
+++ src/main/java/io/raineri/jbow/TextProcessor.java
@@ -0,0 +1,24 @@
+package io.raineri.jbow;
+
+import java.text.Normalizer;
+
+public class TextProcessor {
+    static public String removeNonAlphabetCharsFromWord(String word) {
+        return word.replaceAll("[^a-zA-Z0-9]", "");
+    }
+
+    static public String removeAccentsCharsFromWord(String word) {
+        String normalizedWork = Normalizer.normalize(word, Normalizer.Form.NFD);
+        return normalizedWork.replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+    }
+
+    static public String transformLowerCaseWord(String word) {
+        return word.toLowerCase();
+    }
+
+    static public String cleanWord(String word) {
+        word = TextProcessor.removeAccentsCharsFromWord(word);
+        word = TextProcessor.transformLowerCaseWord(word);
+        return TextProcessor.removeNonAlphabetCharsFromWord(word);     
+    }
+}
